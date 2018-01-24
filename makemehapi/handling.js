5fcd899c9989d5878b19d39ffaa7b57d96ffc889
Erik Wotring <ewotring@gmail.com>
2018-01-24 13:14:39 -0600

    Complete handling exercise

--- makemehapi/handling.js
+++ makemehapi/handling.js
@@ -1,6 +1,58 @@
-var Hapi = require('hapi');
-var Inert = require('inert');
-var server = new Hapi.Server({
-  host: 'localhost',
-  port: Number(process.argv[2] || 8080)
-})
+const Hapi = require('hapi');
+const Inert = require('inert');
+
+(async () => {
+    try {
+        const server = Hapi.Server({
+            host: 'localhost',
+            port: process.argv[2] || 8080,
+            routes: {
+              files: {
+                relativeTo: __dirname
+              }
+            }
+        });
+
+        await server.register(Inert);
+
+        server.route({
+            path: '/',
+            method: 'GET',
+            handler: {
+                file: 'index.html'
+            }
+        });
+
+        await server.start();
+
+        console.log(`Server running at: ${server.info.uri}`);
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+
+// (async () => {
+//   try {
+//     const server = new Hapi.Server({
+//       host: 'localhost',
+//       port: Number(process.argv[2] || 8080)
+//     })
+
+    
+//     await server.register(Inert, (err) => {
+//       if (err) {
+//         throw err;
+//       }
+//       server.route({
+//         path: '/',
+//         method: 'GET',
+//         handler: {
+//           file: 'index.html'
+//         }
+//       });
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
