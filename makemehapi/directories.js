d0748f0f6c417a4c836f736e211d58a915ed58a8
Erik Wotring <ewotring@gmail.com>
2018-01-24 16:31:29 -0600

    Complete directories exercise

---
+++ makemehapi/directories.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Path = require('path');
+
+// (async () => {
+//     try {
+//         const server = Hapi.Server({
+//             host: 'localhost',
+//             port: process.argv[2] || 8080,
+//             routes: {
+//               files: {
+//                 relativeTo: __dirname
+//               }
+//             }
+//         });
+
+//         await server.register(Inert);
+
+//         server.route({
+//             path: '/',
+//             method: 'GET',
+//             handler: {
+//                 file: 'index.html'
+//             }
+//         });
+
+//         await server.start();
+
+//         console.log(`Server running at: ${server.info.uri}`);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })();
+
+(async () => {
+  try {
+    const server = Hapi.Server({
+      host: 'localhost',
+      port: process.argv[2] || 8080
+    });
+
+    await server.register(Inert);
+
+    server.route({
+      method: 'GET',
+      path: '/foo/bar/baz/{param}',
+      handler: {
+        directory: {
+          path: Path.join(__dirname, 'public')
+        }
+      }
+    })
+    
+    await server.start();
+    console.log(`Server running at ${server.info.uri}`);
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+/*
+official solution
+
+    const Path = require('path');
+    const Hapi = require('hapi');
+    const Inert = require('inert');
+
+    (async () => {
+        try {
+            const server = Hapi.Server({
+                host: 'localhost',
+                port: process.argv[2] || 8080
+            });
+
+            await server.register(Inert);
+
+            server.route({
+                path: '/foo/bar/baz/{filename}',
+                method: 'GET',
+                handler: {
+                    directory: {
+                        path: Path.join(__dirname, 'public')
+                    }
+                }
+            });
+
+            await server.start();
+
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+
+    */
