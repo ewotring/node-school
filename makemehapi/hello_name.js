28c1731b973ef692ff4bcc4b0e0b6b8a057f1888
Erik Wotring <ewotring@gmail.com>
2018-01-23 19:19:43 -0600

    Complete rest well with hapi exercise

---
+++ makemehapi/hello_name.js
@@ -0,0 +1,56 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+})
+server.route({
+  path: `/{name}`,
+  method: 'GET',
+  handler: function (request, reply) {
+    return `Hello ${request.params.name} `;
+  }
+});
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
+});
+
+/*
+OFFICIAL SOLUTION
+
+    const Hapi = require('hapi');
+
+    (async () => {
+        try {
+            const server = Hapi.Server({
+                host: 'localhost',
+                port: Number(process.argv[2] || 8080)
+            });
+
+            server.route({
+                path: '/{name}',
+                method: 'GET',
+                handler: function (request, h) {
+                    return `Hello ${request.params.name} `;
+
+                    // a more secure alternative is this:
+                    //
+                    // return `Hello ${encodeURIComponent(request.params.name)}`;
+                    //
+                    // encodeURIComponent escapes all characters except the following: alphabetic, decimal digits, - _ . ! ~ * ' ( )
+                    // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+                    // for more details why you should callencodeURIComponent on any user-entered parameter
+                }
+            });
+
+            await server.start();
+
+            console.log(`Server running at: ${server.info.uri}`);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+
+*/
