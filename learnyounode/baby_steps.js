075c4a7e646fea81c64ee059957ccfaa61dab122
Erik Wotring <ewotring@gmail.com>
2018-04-18 13:16:08 -0500

    Perform Baby Steps exercise

--- learnyounode/baby_steps.js
+++ learnyounode/baby_steps.js
@@ -1,1 +1,14 @@
-console.log(process.argv)
+let totalArg = 0
+for (let i = 2; i < process.argv.length; i++) {
+  totalArg += Number(process.argv[i])
+}
+console.log(totalArg)
+
+// OFFICIAL SOLUTION
+// var result = 0
+
+// for (var i = 2; i < process.argv.length; i++) {
+//   result += Number(process.argv[i])
+// }
+
+// console.log(result)
